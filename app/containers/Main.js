9819c625760ba6b2a668499f22a57ba99b09437b
Nilok Bose <cosmocoder@gmail.com>
2016-04-30 23:22:04 +0530

    Initial commit

---
+++ app/containers/Main.js
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var Hello = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <h1>Hello World!</h1>
+            </div>
+        );
+    }
+});
+
+
+module.exports = Hello;
