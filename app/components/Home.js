c424fb22b28bace149c3e92c002073001e62b116
Nilok Bose <cosmocoder@gmail.com>
2016-05-07 13:03:43 +0530

    Rearranged the code for the home route

---
+++ app/components/Home.js
@@ -0,0 +1,40 @@
+var React = require('react');
+var GetCityContainer = require('../containers/GetCityContainer');
+
+var styles = {
+    outerContainer: {
+        height: '100%',
+        backgroundImage: "url('app/images/pattern.svg')",
+        backgroundSize: 'cover',
+        position: 'relative'
+    },
+
+    innerContainer: {
+        maxWidth: '600px',
+        margin: 'auto',
+        position: 'absolute',
+        top: '50%',
+        left: 0,
+        right: 0,
+        transform: 'translateY(-50%)'
+    },
+
+    heading: {
+        color: '#fff',
+        marginTop: 0
+    }
+};
+
+function Home() {
+    return (
+        <div style={styles.outerContainer}>
+            <div className="container-fluid text-center" style={styles.innerContainer}>
+                <h1 style={styles.heading}>Enter a City and State</h1>
+
+                <GetCityContainer label="Enter city and state" placeholder="St. George, Utah" />
+            </div>
+        </div>
+    );
+}
+
+module.exports = Home;
--- app/containers/HomeContainer.js
+++ app/containers/HomeContainer.js
@@ -1,42 +1,10 @@
 var React = require('react');
-var GetCityContainer = require('./GetCityContainer');
+var Home = require('../components/Home');
 
-var styles = {
-    outerContainer: {
-        height: '100%',
-        backgroundImage: "url('app/images/pattern.svg')",
-        backgroundSize: 'cover',
-        position: 'relative'
-    },
-
-    innerContainer: {
-        maxWidth: '600px',
-        margin: 'auto',
-        position: 'absolute',
-        top: '50%',
-        left: 0,
-        right: 0,
-        transform: 'translateY(-50%)'
-    },
-
-    heading: {
-        color: '#fff',
-        marginTop: 0
-    }
-};
-
-var Home = React.createClass({
+var HomeContainer = React.createClass({
     render: function() {
-        return (
-            <div style={styles.outerContainer}>
-                <div className="container-fluid text-center" style={styles.innerContainer}>
-                    <h1 style={styles.heading}>Enter a City and State</h1>
-
-                    <GetCityContainer label="Enter city and state" placeholder="St. George, Utah" />
-                </div>
-            </div>
-        );
+        return <Home/>;
     }
 });
 
-module.exports = Home;
+module.exports = HomeContainer;
