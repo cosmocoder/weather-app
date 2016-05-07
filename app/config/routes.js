bc0776eef7f6b8e97d0b0cb378e380f533256555
Nilok Bose <cosmocoder@gmail.com>
2016-05-07 12:58:08 +0530

    Renamed route components

--- app/config/routes.js
+++ app/config/routes.js
@@ -5,16 +5,16 @@ var Route = ReactRouter.Route;
 var hashHistory = ReactRouter.hashHistory;
 var IndexRoute = ReactRouter.IndexRoute;
 var Main = require('../containers/Main');
-var Home = require('../containers/HomeContainer');
-var Forecast = require('../containers/ForecastContainer');
-var Detail = require('../containers/DetailContainer');
+var HomeContainer = require('../containers/HomeContainer');
+var ForecastContainer = require('../containers/ForecastContainer');
+var DetailContainer = require('../containers/DetailContainer');
 
 var Routes = (
     <Router history={hashHistory}>
         <Route path="/" component={Main}>
-            <IndexRoute component={Home}/>
-            <Route path="forecast/:city" component={Forecast}/>
-            <Route path="detail/:city" component={Detail}/>
+            <IndexRoute component={HomeContainer}/>
+            <Route path="forecast/:city" component={ForecastContainer}/>
+            <Route path="detail/:city" component={DetailContainer}/>
         </Route>
     </Router>
 );
